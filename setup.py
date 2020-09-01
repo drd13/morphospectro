de7dc00308cfb88098cac7565a6876e260d4b20e
Damien de Mijolla <damiendemijolla@gmail.com>
2020-09-01 16:38:02 +0100

    initial commt

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+        name="morphospectro",
+        version="0.1",
+        author="Damien de Mijolla",
+        author_email="ucapdde@ucl.ac.uk",
+        description="Two stream network for representation learning",
+        packages=setuptools.find_packages()
+        )
+
