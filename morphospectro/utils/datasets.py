1b85d4ab8fbd6c2dcb04ad393ded2321140dda85
Damien de Mijolla <damiendemijolla@gmail.com>
2020-09-04 16:41:16 +0000

    scrappy but working-ish codebase

--- morphospectro/utils/datasets.py
+++ morphospectro/utils/datasets.py
@@ -8,7 +8,7 @@ import skimage.io
 import pandas as pd
 
 class GalaxyDataset(Dataset):
-    def __init__(self,spectra_file,image_folder,debug = False):
+    def __init__(self,spectra_file,image_folder,debug = False,keys_spectra=["flux","obj_ids"],size=None,idx_start=0):
         """
         A pytorch dataset class used for training a neural network. Will automatically generate cutout photometry images based on ra,dec.
         spectra_file: str
@@ -16,30 +16,44 @@ class GalaxyDataset(Dataset):
             
         image_folder: str
             A filepath towards a directory in which images are found. If not existing will be created
+            
+        keys_spectra: list
+            list/tupple where keys[0] is the header name for fluxes and keys[1] for obj_ids
+        
         """
         self.spectra_file = spectra_file
         self.image_folder = image_folder
-        self.flux,self.obj_ids = self.load_spectra(self.spectra_file)
+        self.flux,self.obj_ids = self.load_spectra(self.spectra_file,keys_spectra)
+        self.size = size
+        self.idx_start = idx_start
         self.transform = transforms.Compose([
             torchvision.transforms.ToPILImage(),
-            transforms.ToTensor()]
+            #torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.2),
+            torchvision.transforms.Resize(size=[32,32]),
+            #torchvision.transforms.RandomResizedCrop(size=32,scale=(0.6,1)),
+            torchvision.transforms.Grayscale(),
+            torchvision.transforms.RandomRotation(180),
+            #torchvision.transforms.Resize(size=[28,28]),
+            transforms.ToTensor(),
+            #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ]
         )
         self.debug = debug
 
         
-    def load_spectra(self,spectra_file):
-        flux = pd.read_hdf(spectra_file,key="flux")
-        obj_ids = pd.read_hdf(spectra_file,key="obj_ids")
+    def load_spectra(self,spectra_file,keys=["flux","obj_ids"]):
+        flux = pd.read_hdf(spectra_file,key=keys[0])
+        obj_ids = pd.read_hdf(spectra_file,key=keys[1])
         return flux,obj_ids
         
-        
-    def download_cutout(self,ra,dec,savepath,pixscale=0.3):
+    def download_cutout(self,ra,dec,pixscale=0.1,savepath=None):
         """downloads a cutout image."""
         url = f"https://www.legacysurvey.org/viewer/cutout.jpg?ra={ra}&dec={dec}&layer=sdss&pixscale={pixscale}"
         if self.debug is True:
             print(f"url is {url}")
         img = skimage.io.imread(url)
-        skimage.io.imsave(savepath,img)
+        if savepath:
+            skimage.io.imsave(savepath,img)
         return img
 
     def get_spectra(self,idx):
@@ -50,8 +64,9 @@ class GalaxyDataset(Dataset):
         dec = self.obj_ids.loc[idx]["dec"]
         dr8objid = int(self.obj_ids.loc[idx]["dr8objid"])
         return ra,dec,dr8objid
+    
    
-    def get_image(self,idx):
+    def get_image(self,idx,pixscale=0.1):
         ra,dec,objid = self.get_params(idx)
         image_name = f"{objid}.jpg"
         image_path = os.path.join(self.image_folder,image_name)
@@ -60,15 +75,25 @@ class GalaxyDataset(Dataset):
             return skimage.io.imread(image_path)
         except:
             print("failed")
-            return self.download_cutout(ra,dec,image_path)
+            return self.download_cutout(ra,dec,savepath=image_path)
    
     def __len__(self):
-        return len(self.flux)
+        if self.size is None:
+            return len(self.flux)
+        else:
+            return self.size
     
     
     def __getitem__(self,idx):
+        idx = self.idx_start+idx
         spectra = self.get_spectra(idx)
         image = self.get_image(idx)
         image = self.transform(image)
 
         return spectra,image,idx
+
+    
+
+    
+    
+    
--- morphospectro/utils/networks.py
+++ morphospectro/utils/networks.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 
 class Feedforward(nn.Module):
-    def __init__(self,structure,activation=nn.LeakyReLU(),with_dropout=False):
+    def __init__(self,structure,activation=nn.LeakyReLU(),with_dropout=False,final_activation=False):
         super(Feedforward, self).__init__()        
         self.layers = []
         for i in range(len(structure)-2):
@@ -18,6 +18,9 @@ class Feedforward(nn.Module):
             self.layers.append(activation)
         
         self.layers.append(nn.Linear(structure[-2],structure[-1]))
+        if final_activation is True:
+            self.layers.append(activation)
+            
         self.fc = nn.Sequential(*self.layers)
 
 
@@ -27,3 +30,165 @@ class Feedforward(nn.Module):
         #    x = layer(x)
         x = self.fc(x)
         return x
+    
+    
+class FullNet(nn.Module):
+    def __init__(self,spec_network,im_network,merged_network):
+        super(FullNet, self).__init__()      
+        self.im_network = im_network
+        self.spec_network = spec_network
+        self.merged_network = merged_network
+
+
+    def forward(self, spec, im):
+        latent_im  = self.im_network(im)
+        latent_spec  = self.spec_network(spec)
+        #merged_latent = latent_im-latent_spec
+        #pred_match = self.merged_network(merged_latent)
+        #print(latent_im.shape)
+        merged_latent = torch.cat((latent_im,latent_spec),dim=1)
+        #print(merged_latent.shape)
+        pred_match = self.merged_network(merged_latent)
+        return pred_match
+    
+    
+class ConvNetBasic(nn.Module):
+
+    def __init__(self,n_channels = 3):
+        super(ConvNetBasic, self).__init__()
+        # 1 input image channel, 6 output channels, 3x3 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(n_channels, 12, 3)
+        self.conv2 = nn.Conv2d(12, 24, 3)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(864, 120)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(120, 50)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        return x
+
+    
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+    
+    
+
+    
+    
+class ConvNet(nn.Module):
+    def __init__(self,n_channels=3):
+        super(ConvNet, self).__init__()
+        # 1 input image channel, 6 output channels, 3x3 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(n_channels, 8,4,stride=2)
+        self.conv2 = nn.Conv2d(8, 16, 4,stride=1)
+        self.conv3 = nn.Conv2d(16, 32, 4,stride=1)
+
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(5408, 512)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 50)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv3(x)), 2)
+
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+    
+    
+
+    
+    
+    
+
+class SpectraEmbedding(nn.Module):
+    def __init__(self,ndf):
+        super(SpectraEmbedding,self).__init__()
+        nc=1
+        # 4096
+        self.layer1 = nn.Sequential(nn.Conv1d(nc,ndf,kernel_size=10,stride=4,padding=3),
+                                 nn.BatchNorm1d(ndf),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        # 1024
+        self.layer2 = nn.Sequential(nn.Conv1d(ndf,ndf*2,kernel_size=10,stride=4,padding=3),
+                                 nn.BatchNorm1d(ndf*2),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        # 256
+        self.layer3 = nn.Sequential(nn.Conv1d(ndf*2,ndf*4,kernel_size=10,stride=4,padding=3),
+                                 nn.BatchNorm1d(ndf*4),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        # 64
+        self.layer4 = nn.Sequential(nn.Conv1d(ndf*4,ndf*4,kernel_size=10,stride=4,padding=3),
+                                 nn.BatchNorm1d(ndf*4),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        #16
+        self.layer5 = nn.Sequential(nn.Conv1d(ndf*4,16,kernel_size=16,stride=1,padding=0),
+                                 nn.LeakyReLU(0.2,inplace=True))
+
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        return out
+
+
+
+class ImageEmbedding(nn.Module):
+    def __init__(self,ndf):
+        super(ImageEmbedding,self).__init__()
+        nc=3
+        # 32 x 32
+        self.layer1 = nn.Sequential(nn.Conv2d(nc,ndf,kernel_size=4,stride=2,padding=1),
+                                 nn.BatchNorm2d(ndf),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        # 16 x 16
+        self.layer2 = nn.Sequential(nn.Conv2d(ndf,ndf*2,kernel_size=4,stride=2,padding=1),
+                                 nn.BatchNorm2d(ndf*2),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        # 8 x 8
+        self.layer3 = nn.Sequential(nn.Conv2d(ndf*2,ndf*4,kernel_size=4,stride=2,padding=1),
+                                 nn.BatchNorm2d(ndf*4),
+                                 nn.LeakyReLU(0.2,inplace=True))
+        # 4 x 4
+        self.layer4 = nn.Sequential(nn.Conv2d(ndf*4,16,kernel_size=4,stride=1,padding=0),
+                                 nn.LeakyReLU(0.2,inplace=True))
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        return out
+
+    
+
+    
